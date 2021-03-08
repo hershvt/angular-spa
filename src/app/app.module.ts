e7a52c826ecb215c09d52e064465c78a328deb76
hersvt <hershtandon30@gmail.com>
2021-03-09 00:12:36 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ServicesComponent } from './services/services.component';
+import { BusinessSolutionComponent } from './business-solution/business-solution.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomePageComponent,
+    FooterComponent,
+    AboutUsComponent,
+    ServicesComponent,
+    BusinessSolutionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/business-solution/business-solution.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-business-solution',
+  templateUrl: './business-solution.component.html',
+  styleUrls: ['./business-solution.component.css']
+})
+export class BusinessSolutionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
